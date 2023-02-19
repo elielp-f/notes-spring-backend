d58e330fb3f5f457bba929079b85ebd97b4adc75
Eliel P. Ferreira <eliel.dev@hotmail.com>
2023-02-19 01:14:49 -0300

    Note Controller

--- src/main/java/com/notes/notes/NoteController.java
+++ src/main/java/com/notes/notes/NoteController.java
@@ -1,10 +1,30 @@
 package com.notes.notes;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class NoteController {
-    public Note get(){
+    @GetMapping("/note/{id}")
+    public Note get(@RequestParam String id){
+        return new Note();
+    }
+
+    @GetMapping("/notes/{authorId}")
+    public List<Note> getNotesByAuthor(@RequestParam String authorId){
+        List<Note> notes = new ArrayList<>();
+        return notes;
+    }
+
+    @PostMapping("/note/edit")
+    public Note editNote(@RequestBody Note updateNote, @RequestBody String id){
+        return  new Note();
+    }
+
+    @PostMapping("/note")
+    public Note createNote(@RequestBody Note note){
         return new Note();
     }
 }
